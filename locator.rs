640c9cb433ea13c5bfa68c9962b44f276ba49377
Zachariah Zachariah <zachgit1999@gmail.com>
2023-07-13 19:12:23 -0400

    Update locator.rs

--- locator.rs
+++ locator.rs
@@ -8,7 +8,7 @@ pub trait PriorityQueue<T: PartialOrd> {
 }
 
 /**
-    An optional definition of a Node struct you may find useful
+    Definition of a Node struct
 **/
 struct Node<T> {
     priority: i32,
@@ -16,7 +16,7 @@ struct Node<T> {
 }
 
 /** 
-    These traits are implemented for Nodes to make them comparable 
+    Traits implemented for Nodes to make them comparable 
 **/
 impl<T> PartialOrd for Node<T> {
     fn partial_cmp(&self, other: &Node<T>) -> Option<Ordering> {
@@ -35,16 +35,10 @@ fn leftchild(i: i32)-> i32  { (2*i)+1 }
 fn rightchild(i: i32)-> i32 { (2*i)+2 }
 
 
-
-/** 
-    You must implement the above trait for the vector type 
-**/
 impl<T: PartialOrd> PriorityQueue<T> for Vec<T> {
     /**
         This functions pushes a given element onto the queue and
         reorders the queue such that the min heap property holds.
-        See the project specifications for more details on how this
-        works.
     **/
     fn enqueue(&mut self, ele: T) -> () {
     
@@ -63,9 +57,8 @@ impl<T: PartialOrd> PriorityQueue<T> for Vec<T> {
     /**
         This function removes the root element from the queue and
         reorders the queue such that it maintains the min heap
-        property.  See the project specifications for more details.
-        You should return the deleted element in the form of an option.
-        Return None if the queue was initially empty, Some(T) otherwise.
+        property. Returns the deleted element in the form of an option. 
+        Return None if the queue was initially empty, Some(T) otherwise. 
     **/
     fn dequeue(&mut self) -> Option<T> {
        
@@ -104,8 +97,7 @@ impl<T: PartialOrd> PriorityQueue<T> for Vec<T> {
 
     /**
         This function returns the element that would be removed
-        if dequeue were called on the queue.  There should be no
-        mutations to the queue.  Return the element in the form
+        if dequeue were called on the queue. Return the element in the form
         of an option.  Return None if the queue is empty, Some(T)
         otherwise.
     **/
@@ -125,10 +117,7 @@ impl<T: PartialOrd> PriorityQueue<T> for Vec<T> {
 
 
 /**
-    You must implement this function that computes the orthogonal
-    distance between two coordinates.  Remember, orthogonal distance
-    is not like Euclidean distance.  See the specifications for more
-    details.
+    Function computes the orthogonal distance between two coordinates. 
 **/
 pub fn distance(p1: (i32,i32), p2: (i32,i32)) -> i32 {
     let (x1,y1) = p1;
@@ -140,13 +129,11 @@ pub fn distance(p1: (i32,i32), p2: (i32,i32)) -> i32 {
 }
 
 /**
-    You must implement this function that determines which enemy Stark
-    should battle and their coordinates.  You are given two hashmaps for
-    allies and enemies.  Each maps a name to their current coordinates.
-    You can assume that the allies hashmap will always have a name
-    called "Stark" included.  Return the name and coordinates of the enemy
-    Stark will battle in the form of a 3-tuple.  See the specifications
-    for more details on how to choose which enemy.
+    Function determines which enemy Stark should battle and their coordinates.
+    Two hashmaps are given for allies and enemies. Each maps a name to their 
+    current coordinates. The allies hashmap will always have a name
+    called "Stark" included.  Returns the name and coordinates of the enemy
+    Stark will battle in the form of a 3-tuple.  
 **/
 pub fn target_locator<'a>(allies: &'a HashMap<&String, (i32,i32)>, enemies: &'a HashMap<&String, (i32,i32)>) -> (&'a str,i32,i32) {
 
